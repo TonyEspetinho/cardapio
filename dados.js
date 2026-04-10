b01ae6a72b2980d61949fb501930b5e9072c5d86
TonyEspetinho <tony2100pereira@gmail.com>
2026-04-10 16:50:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,103 @@
+const categorias = [
+
+  {
+    nome: "🔥 Espetos",
+    adicionais: true,
+    itens: [
+      { nome: "Boi (Alcatra)", preco: 12 },
+      { nome: "Porco", preco: 12 },
+      { nome: "Medalhão", preco: 12 },
+      { nome: "Coração", preco: 12 },
+      { nome: "Linguiça", preco: 12 },
+      { nome: "Kafta da Casa", preco: 12 },
+      { nome: "Kafta com Queijo", preco: 12 },
+    ]
+  },
+
+  {
+    nome: "🍛 Lanches com Churrasco",
+    itens: [
+      { nome: "Pão com Carne", preco: 19 },
+      { nome: "Macarrão", preco: 24 },
+      { nome: "Feijão Tropeiro", preco: 24 },
+      { nome: "Jantinha (acompanha espetinho)", preco: 24 },
+      { nome: "Cê Tá Doido (espeto + 2 ovos + bacon + queijo + salada)", preco: 30 },
+    ]
+  },
+
+  {
+    nome: "🍔 Lanche Artesanal",
+    itens: [
+      { nome: "X-Burguer", preco: 21 },
+      { nome: "X-Bacon Burguer", preco: 26 },
+      { nome: "X-Egg Bacon", preco: 29 },
+    ]
+  },
+
+
+
+  {
+    nome: "🥤 Bebidas",
+    itens: [
+      { nome: "Suco Natural", preco: 12 },
+      { nome: "Refrigerante 2L", preco: 15 },
+      { nome: "Refrigerante 1L", preco: 12 },
+      { nome: "Refrigerante 600ml", preco: 10 },
+      { nome: "Refrigerante Lata", preco: 7 },
+      { nome: "Refrigerante Mini", preco: 5 },
+      { nome: "Água", preco: 3.5 },
+      { nome: "Água com Gás", preco: 4.5 },
+      { nome: "Limoneto", preco: 8 },
+      { nome: "Chopp de Vinho", preco: 12 },
+      { nome: "Beats", preco: 12 },
+      { nome: "Energético", preco: 12 },
+      { nome: "Red Bull", preco: 15 },
+    ]
+  },
+
+  {
+    nome: "🍹 Drinks",
+    itens: [
+      { nome: "Caipirinha", preco: 15 },
+      { nome: "Caipivodka", preco: 20 },
+      { nome: "Copão", preco: 25 },
+    ]
+  },
+
+  {
+    nome: "🍺 Cervejas",
+    itens: [
+      { nome: "Brahma", preco: 9 },
+      { nome: "Amstel", preco: 9 },
+      { nome: "Original", preco: 10 },
+      { nome: "Skol", preco: 8 },
+      { nome: "Heineken", preco: 10 },
+      { nome: "Spaten", preco: 10 },
+      { nome: "Kaiser", preco: 8 },
+    ]
+  },
+
+  {
+    nome: "🥃 Doses",
+    itens: [
+      { nome: "Vodka", preco: 8 },
+      { nome: "Whisky", preco: 10 },
+      { nome: "Rum", preco: 8 },
+      { nome: "Catuaba", preco: 8 },
+      { nome: "Canelinha", preco: 8 },
+      { nome: "Licor Artesanal", preco: 8 },
+    ]
+  }
+
+];
+
+const adicionais = [
+  { nome: "Cebola", preco: 0.5 },
+  { nome: "Tomate", preco: 0.5 },
+  { nome: "Molho", preco: 0 },
+  { nome: "Farinha", preco: 0 },
+];
+
+const bairros = [
+  { nome: "Amaro Lanari", taxa: 0 }
+];
